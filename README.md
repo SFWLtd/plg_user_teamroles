3fe32cbcba52214361e6d21e7550b5b58fa9ebb0
Simon Champion <simonc@sfwltd.co.uk>
2016-08-08 15:22:57 +0100

    Initial commit to Github.

---
+++ README.md
@@ -0,0 +1,74 @@
+User TeamRoles: Parent/Child synched between Joomla Groups and Moodle Mentees via Joomdle
+=========================================================================================
+
+The aim of the plugin is to allow your users to be organised in Joomla into a hierarchy of teams with designated team leaders, and for those teams and team leader roles to be reflected in Moodle via its parent/child (aka mentor/mentee) relationships.
+
+Unlike other similar plugins that work with Joomdle, it does not rely on any other Joomla social plugins; it simply uses Joomla's standard user group and access levels functionality.
+
+
+Dependencies:
+-------------
+
+* Joomla (http://joomla.org/)
+* Moodle (http://moodle.org/)
+* Joomdle (http://www.joomdle.com) (>v1.0.6; see note below)
+
+
+Notes:
+------
+
+* NB: This plugin uses an API method (remove_parent_role) that was only added to Joomdle v1.0.6. You should ensure that your Joomdle version is 1.0.6 or higher.
+
+* As far as Joomla is concerned, this is a User plugin not a Joomdle plugin. This is because hooks into the events triggered by Joomla's User component. It does not hook into any Joomdle events. Instead it uses Joomdle's API to make direct calls to the linked Moodle system. It therefore requires Joomdle, but will be listed in Joomla as a User plugin.
+
+Installation:
+-------------
+
+1. Install Joomla, Moodle, and Joomdle.
+2. In Moodle, create a parent role with appropriate permissions (see https://docs.moodle.org/30/en/Parent_role for instructions), and define it as the parent in the Joomdle config (go to Site Admin/Plugins/Auth/Joomdle, and populate the "Parent Role ID" field with the ID of the role you just created).
+3. In Joomla, create a user group structure: One group for the parents, and another group with multiple nested groups for the teams. See below for more detail on this.
+4. In Joomla, Create an access level for the parents; link it to the parents group you created above.
+5. Install the plugin to Joomla using whatever method is most convenient.
+6. Configure plugin: Find the plugin config page in Extensions/Plugins. Once there, set the "Access for 'Parent' role" to the access level created in step 3, and the "Top-level group for sync" to the parent group containing all the team groups. Activate the plugin by setting the status to Enabled, and press save.
+
+You should now be able to create Joomla users in your team groups and in the parent group, and the plugin will automatically update the Moodle parent/child relationships for those users. You can then use Moodle features and plugins (like the built-in My Mentees block, or the ones listed here: https://moodle.org/plugins/search.php?s=mentee&search=Search+plugins) to allow the parents to see management details about their children.
+
+
+Tip: We have used this in conjunction with the [Multi Usergroup Registration plugin](http://extensions.joomla.org/extensions/extension/clients-a-communities/user-management/multi-usergroup-registration) which means we can allow end users to specify which team they belong to when they create their account.
+
+
+Group Structure and Access Levels:
+----------------------------------
+
+The plugin is designed to work with a group structure that looks like this in Joomla:
+
+    - Team Leaders
+    - Teams
+      |- Team 1
+      |- Team 2
+      |- Team 3
+      |- etc...
+
+You would also define a Joomla Access Level for Team Leader, which should be granted to the Team Leaders group.
+
+The plugin config should be set with the "Teams" group as the "top-level group", and the "Team Leader" access level as the access level for the parent role.
+
+With the groups and the access level in place, and the plugin configured, you are now ready to start organising your users.
+
+Your users should be allocated into their group within Teams (in our case, we are using the Joomla Multi-usergroup Registration plugin (http://extensions.joomla.org/extensions/extension/clients-a-communities/user-management/multi-usergroup-registration) to allow users to select their own group, but you can add users to groups in whatever way suits your use-case). Team leaders would be members of their group in the same way as other users, but would additionally be added to the Team Leaders group. This would then give them the team leader access level, which would in turn trigger Joomdle to give them parent role over the rest of the team in Moodle.
+
+
+Caveats and Limitations:
+------------------------
+
+The plugin works well for the use-case for which it was designed. However, if you intend to implement it for your site, you may need to note the following:
+
+* No nested teams: The plugin code looks at the list of groups that are immediate child groups of the top-level group. It does not recurse any further down the group tree. This means that the plugin does not support any kind of nested group structure within the team groups.
+
+* Multiple groups: You may want to set users and team leaders to be members of multiple teams. This is possible, but has limitations and has not been properly tested. The most obvious limitation is that there's only one team leader flag, so you can't be a leader of one group without being a leader of all other groups that you belong to. Also, since the group structure does not carry over to Moodle; just the parent-child relationships between team leaders and other users, a team leader who is in multiple groups will see all the users he is responsible for in his Mentees list, but nothing to tell him which groups they belong to.
+
+* Large number of teams: There are reports on various forums that Joomla has been known to have performance issues when it has a large number of groups. Please bear this in mind if you are likely to need a lot of groups.
+
+* This plugin has only been tested against the current versions of Joomla (3.4.8) , Moodle (3.0.2+) and Joomdle (1.0.5) as at the time of writing. Provided that Joomla's plugin API remains the same and that Joomdle itself works, there is no reason why it shouldn't work in other versions, but if you are using other versions, please be aware that it has not been tested, and respond accordingly.
+
+* Be sure that your Joomdle version includes the remove_parent_role method. See notes section above.
---
+++ teamroles.php
@@ -0,0 +1,221 @@
+<?php
+/**
+* @version		1.0
+* @package		plg_user_teamroles
+* @author       Simon Champion
+* @copyright	SFW Ltd
+* @license		http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+* @requires     Joomdle >= 1.0.6
+*/
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+require_once(JPATH_SITE.'/components/com_joomdle/helpers/content.php');
+
+class plgUserTeamRoles extends JPlugin
+{
+    const MOODLE_CONTEXT_USER = 30;
+
+    private $userInfo = null;
+
+    public function onUserBeforeSave($oldUser, $isNew, $newUser)
+    {
+        $this->userInfo = new TeamRolesUserInfo($this->params, $oldUser['id'], $oldUser['username']);
+    }
+
+    public function onUserAfterSave($user, $isnew, $success, $msg)
+    {
+        if (!$success) {
+            return;
+        }
+        JAccess::clearStatics();
+        $this->userInfo->addComparitor(new TeamRolesUserInfo($this->params, $user['id'], $user['username']));
+        $this->updateRoles();
+    }
+
+    private function updateRoles()
+    {
+        if ($this->userInfo->sameAsComparitor()) {
+            //nothing changed, so bail out early.
+            return;
+        }
+        foreach ($this->userInfo->groupsAdded() as $group) {
+            if ($this->userInfo->updated->iAmAParent) {
+                $this->giveMeParentRoleForGroup($group);
+            } else {
+                $this->giveMeChildRoleForGroup($group);
+            }
+        }
+        foreach ($this->userInfo->groupsRemoved() as $group) {
+            if ($this->userInfo->iAmAParent) {
+                $this->removeMyParentRoleForGroup($group);
+            } else {
+                $this->removeMyChildRoleForGroup($group);
+            }
+        }
+        if ($this->userInfo->isParentRoleChanging()) {
+            foreach ($this->userInfo->groupsUnchanged() as $group) {
+                if ($this->userInfo->isBecomingAParent()) {
+                    $this->giveMeParentRoleForGroup($group);
+                }
+                if ($this->userInfo->isNoLongerAParent()) {
+                    $this->removeMyParentRoleForGroup($group);
+                }
+            }
+        }
+    }
+
+    /**
+     * User has been been given group leader rights, so need to give him parent roles for everyone in his group.
+     * So look at all the other users in the group and grant him parent connection to them.
+     */
+    private function giveMeParentRoleForGroup($group)
+    {
+        foreach ($this->usersInGroup($group) as $user) {
+            $this->joomdleAddParentRole(JFactory::getUser($user)->get('username'), $this->userInfo->username);
+        }
+    }
+
+    /**
+     * User has been had his group leader rights revoked, so need to unpick all of his parent roles.
+     * So look at all the other users in the group and remove his parent connection to them.
+     */
+    private function removeMyParentRoleForGroup($group)
+    {
+        foreach ($this->usersInGroup($group) as $user) {
+            $this->joomdleRemoveParentRole(JFactory::getUser($user)->get('username'), $this->userInfo->username);
+        }
+    }
+
+    /**
+     * User has been added to a group, so need to implement the parent roles for that group to point to him.
+     * So look at the other users in the group, from them, anyone who is marked as having the privs need to gain parent connection with this user.
+     */
+    private function giveMeChildRoleForGroup($group)
+    {
+        foreach ($this->usersInGroup($group) as $user) {
+            if (in_array($this->userInfo->configAccess, JAccess::getAuthorisedViewLevels($user))) {
+                $this->joomdleAddParentRole($this->userInfo->username, JFactory::getUser($user)->get('username'));
+            }
+        }
+    }
+
+    /**
+     * User has been removed from a group, so need to unpick the parent roles pointing to him that are associated with that group.
+     * So look at the other users in the group, from them, anyone who is marked as having the privs need to lose the parent connection with this user.
+     */
+    private function removeMyChildRoleForGroup($group)
+    {
+        foreach ($this->usersInGroup($group) as $user) {
+            if (in_array($this->userInfo->configAccess, JAccess::getAuthorisedViewLevels($user))) {
+                $this->joomdleRemoveParentRole($this->userInfo->username, JFactory::getUser($user)->get('username'));
+            }
+        }
+    }
+
+    private function joomdleAddParentRole($child, $parent)
+    {
+        JoomdleHelperContent::call_method('add_parent_role', $child, $parent);
+    }
+    private function joomdleRemoveParentRole($child, $parent)
+    {
+        //NB: remove_parent_role is not part of the standard Joomdle package in current version (1.0.5). Should be in place for 1.0.6, but please ensure method exists.
+        JoomdleHelperContent::call_method('remove_parent_role', $child, $parent);
+    }
+
+    private function usersInGroup($group)
+    {
+        return JAccess::getUsersByGroup($group);
+    }
+
+}
+
+//-----------------------------------------------------------------------------
+
+class TeamRolesUserInfo
+{
+    public $userID = 0;
+    public $username = '';
+
+    public $userGroups = [];
+    public $iAmAParent = false;
+
+    public $updated = null;
+
+    public $configGroups;
+    public $configAccess;
+
+    public function __construct($params, $userID, $username)
+    {
+        if (!$userID) {
+            return;
+        }
+
+        $this->userID = $userID;
+        $this->username = $username;
+
+        $this->configGroups = $this->subGroupsOfConfigTopGroup($params->get('topgroup'));
+        $this->configAccess = $params->get('parent');
+
+        //get the users groups, and filter it to only include the ones that are configured for parent/child relationships.
+        $this->userGroups   = array_intersect(JAccess::getGroupsByUser($userID), $this->configGroups);
+
+        //check whether the user has parent access level
+        $this->iAmAParent   = in_array($this->configAccess, JAccess::getAuthorisedViewLevels($userID));
+    }
+
+    /**
+     * Get all subgroups of a given usergroup.
+     * (I'm sure there must be a standard Joomla way of doing this but for the life of me I can't find it)
+     */
+    private function subGroupsOfConfigTopGroup($topGroup)
+    {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select($db->quoteName('id'));
+        $query->from($db->quoteName('#__usergroups'));
+        $query->where($db->quoteName('parent_id').'='.(int)$topGroup);
+
+        $db->setQuery($query);
+        return $db->loadColumn();
+    }
+
+    public function addComparitor(TeamRolesUserInfo $updated)
+    {
+        $this->updated = $updated;
+    }
+
+    public function sameAsComparitor()
+    {
+        return !($this->isParentRoleChanging() || $this->isGroupListChanging());
+    }
+    public function isParentRoleChanging()
+    {
+        return ($this->iAmAParent != $this->updated->iAmAParent);
+    }
+    public function isBecomingAParent()
+    {
+        return (!$this->iAmAParent && $this->updated->iAmAParent);
+    }
+    public function isNoLongerAParent()
+    {
+        return ($this->iAmAParent && !$this->updated->iAmAParent);
+    }
+    public function isGroupListChanging()
+    {
+        return ($this->userGroups != $this->updated->userGroups);
+    }
+
+    public function groupsAdded()
+    {
+        return array_diff($this->updated->userGroups, $this->userGroups);
+    }
+    public function groupsRemoved()
+    {
+        return array_diff($this->userGroups, $this->updated->userGroups);
+    }
+    public function groupsUnchanged()
+    {
+        return array_intersect($this->userGroups, $this->updated->userGroups);
+    }
+}
